4563b078e9fc1ef5e1ba68e09a57705db9daa447
Xavier Gschwind <xaviergschwind@gmail.com>
2022-11-17 15:24:00 -0500

    RunSnapshot ready

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-from sys import argv, stdout
+from sys import argv
 import os
 import string
-byteSize = os.stat(argv[1]).st_size
-inFile = open(argv[1], 'r')
-contents = inFile.read()
+from texttable import Texttable
+t = Texttable()
 
-output = open(argv[1] + '.lwz', 'w')
+byteSize = 0
+inFile = None
+contents = ""
+
+output = None
 compFile = None
 compContents = ""
-decomFile = open('Decompressed-' + argv[1], 'w')
+decomFile = None
 
 # Dictionary based on Ascii between A to z
 def initCodeTable():
     idx = 0
     codeTable = {}
+
+    # Accepts only Ascii letters in dictionary
     for c in list(string.ascii_letters):
-        codeTable[str(idx)] = c # remember to fully convert to values
+        codeTable[str(idx)] = c
         idx += 1
-    #print(codeTable)
             
     return idx, codeTable
 
@@ -27,24 +31,18 @@ def compress():
 
     buffer = contents[0]
     for c in contents[1:]:
-        #print('Buffer and c: ', buffer, c)
         temp = buffer + c
-        if temp in codeTable.values(): # remember to fully convert to values 
+        if temp in codeTable.values():
             buffer = temp
         else:
-            #print('Writing buffer code: ', buffer, str(list(codeTable.values()).index(buffer)))
             output.write(str(list(codeTable.values()).index(buffer)) + ' ')
-           # print('Storing: ', temp, idx)
             codeTable[str(idx)] = temp
             idx += 1
             buffer = c
 
-        #print('Buffer is now: ', buffer)
-    #print('Writing buffer code', buffer, str(list(codeTable.values()).index(buffer)))
     output.write(str(list(codeTable.values()).index(buffer)))
-    #print(codeTable)
 
-def decompress():
+def decompress(file):
     codeTable = {}
     idx, codeTable = initCodeTable()
 
@@ -54,15 +52,13 @@ def decompress():
 
     s = codeTable[prior]
     temp = ''
-    #print(codeTable)
-    decoded = ''
+    decoded = '' # Replaces print steps of algorithm
+
     #stdout.write(s)
     decoded += s
     
 
     for current in listOfContents[1:]:
-        #print('Current: ', current)
-        #print(current, current in codeTable)
         if not current in codeTable:
             c = codeTable[prior][0]
             temp = codeTable[prior] + c
@@ -77,39 +73,40 @@ def decompress():
             idx += 1
             #stdout.write(codeTable[current])
             decoded += codeTable[current]
+
         prior = current
+        
     decomFile.write(decoded)
 
     decomFile.close()
-    compressedSize = os.stat(argv[1] + '.lwz').st_size
-    decompSize = os.stat('Decompressed-' + argv[1]).st_size
-    #print("Same After decoding? ", decoded == contents)
-    print("Input Filename | # bytes in input file | # bytes in compressed file | # bytes in output file | Matching Files? | Compression Efficiency")
-    print(argv[1], '|', byteSize, '|', compressedSize , '|', decompSize, '|', decoded == contents, '|', str(compressedSize/byteSize*100) + '%')
-    #s = codeTable.
-    #for c in compContents:
-
-    # priorIndex = 0
-    # print(prior)
-    # current = ""
-    # while True:
-    #     current = compContents[idx]
-    #     idx+= 1
-    #     if not current in codeTable.keys():
-    #         c = compContents.find(prior, priorIndex)
-    #         temp = compContents.find(prior, priorIndex) + c
-    #         print(temp)
+    compressedSize = os.stat(file + '.lwz').st_size
+    decompSize = os.stat('Decompressed-' + file).st_size
+   
+    status = 'yes' if decoded == contents else 'no'
+    t.add_row([file, byteSize, compressedSize , decompSize, status, str(round(compressedSize/byteSize*100, 4)) + '%'])
 
 
+
+t.add_row(['Input Filename', '# bytes in input file', '# bytes in compressed file', '# bytes in output file', 'Matching Files?', 'Compression Efficiency'])
+
+for file in argv[1:]:
+
+    byteSize = os.stat(file).st_size
+    inFile = open(file, 'r') 
+    contents = inFile.read()
+
+    output = open(file + '.lwz', 'w')
+    decomFile = open('Decompressed-' + file, 'w')
+
+    compress()
+
+    inFile.close()
+    output.close()
+
+    compFile = open(file + '.lwz', 'r')
+    compContents = compFile.read()
+    compFile.close()
+
+    decompress(file)
     
-
-compress()
-
-inFile.close()
-output.close()
-
-compFile = open(argv[1] + '.lwz', 'r')
-compContents = compFile.read()
-compFile.close()
-
-decompress()
+print(t.draw())
